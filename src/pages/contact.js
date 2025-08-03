9abf31a1c02e7a48d3034dceb2c0ca8e333ca7e1
Ken Tsukamoto <tsukaken0622@gmail.com>
2025-08-04 02:13:50 +0900

    ✨ ページの増築

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,7 +7,7 @@ import Head from "next/head";
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"],});
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"],});
 
-export default function Home() {
+export default function Contact() {
   const settings = {dots: true,};
   return (
     <div className={`${geistSans.className} ${geistMono.className} 
@@ -29,8 +29,7 @@ export default function Home() {
       </header>
       <div className="flex-col items-center text-center w-full">
           <div>
-            <Image className="dark:invert" src="/img/2.png" alt="logo" width={1920} height={800} priority/>
-            <h1 className="text-6xl z-2">Index Page</h1>
+            <h1 className="text-6xl z-2">Contact Page</h1>
           </div>
           <div>
             <ol className="font-mono list-inside list-decimal text-sm/6 text-center">
@@ -39,24 +38,10 @@ export default function Home() {
             </ol>
           </div>
         </div>
-        <section className="text-gray-700" id="home">
-          <div className="flex container mx-auto py-20 px-30 flex-col md:flex-row items-center justify-center">
-            <div className="md:w-1/2 flex-grow mb-16 lg:pr-24 md:pr-16 items-center text-center md:text-left">
-              <p className="text-2xl font-bold text text-red-400">
-                次回の定期演奏会
-              </p>
-              <h1 className="text-3xl font-extrabold text-red-700">
-              <span className="text-2xl font-extrabold text-black">令和7年11月20日(木)</span><br />名古屋市<br />電気文化会館<br />ザ・コンサートホール 
-              </h1>
-              <button href="#" className="text-white font-bold bg-green-500 mt-3 py-2 px-3 border-0 rounded text-lg hover:bg-green-400 duration-300">詳細はこちら</button>
-            </div>
-            <div className="md:w-1/2 lg:max-w-lg w-5/6">
-              <img src="/img/paper.png" alt="パンフレット" width={1080} height={1542} />
-            </div>
-            </div>
-        </section>
+       
+       
       <footer>
-          <div className='flex pt-7 pb-12 border-t border-green-200  w-full pl-[6vw] lg:pl-[10vw] text-[2vw] font-extrabold '>
+          <div className='flex pt-7 pb-12 border-t border-green-200  w-full pl-[6vw] lg:pl-[10vw] text-[2vw] '>
               <Link className="flex " href="/"><Image className="mr-1" src="/img/icon.ico" alt="icon" width={10} height={10}/>ホーム</Link>
               <Link className="flex ml-5" href="/about"><Image className="mr-1" src="/img/icon.ico" alt="icon" width={10} height={10}/>リリエンワイスについて</Link>
               <Link className="flex ml-5" href="/music"><Image className="mr-1" src="/img/icon.ico" alt="icon"  width={10} height={10}/>過去の演奏会の音源</Link>
--- src/pages/schedule.js
+++ src/pages/schedule.js
@@ -7,7 +7,7 @@ import Head from "next/head";
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"],});
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"],});
 
-export default function Home() {
+export default function Schedule() {
   const settings = {dots: true,};
   return (
     <div className={`${geistSans.className} ${geistMono.className} 
@@ -29,10 +29,10 @@ export default function Home() {
       </header>
       <div className="flex-col items-center text-center w-full">
           <div>
-            <Image className="dark:invert" src="/img/2.png" alt="logo" width={1920} height={800} priority/>
-            <h1 className="text-6xl z-2">Index Page</h1>
+            <Image className="dark:invert" src="/img/2.png" alt="logo" width={1080} height={400} priority/>
+            <h1 className="text-6xl z-2">Schedule Page</h1>
           </div>
-          <div>
+      <div>
             <ol className="font-mono list-inside list-decimal text-sm/6 text-center">
               <li className="mb-2 tracking-[-.01em]">Get started by editing{" "}<code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">src/pages/index.js</code>.</li>
               <li className="tracking-[-.01em]">Save and see your changes instantly.</li>
@@ -41,22 +41,24 @@ export default function Home() {
         </div>
         <section className="text-gray-700" id="home">
           <div className="flex container mx-auto py-20 px-30 flex-col md:flex-row items-center justify-center">
-            <div className="md:w-1/2 flex-grow mb-16 lg:pr-24 md:pr-16 items-center text-center md:text-left">
+            <div className="md:w-1/2 flex-grow mb-16 lg:pr-24 md:pr-16 text-center md:text-left">
               <p className="text-2xl font-bold text text-red-400">
                 次回の定期演奏会
               </p>
-              <h1 className="text-3xl font-extrabold text-red-700">
-              <span className="text-2xl font-extrabold text-black">令和7年11月20日(木)</span><br />名古屋市<br />電気文化会館<br />ザ・コンサートホール 
+              <h1 className="text-4xl font-extrabold text text-red-700 p-3">
+                名古屋市電気文化会館
+                <br />
+                11月20日(木) 
               </h1>
-              <button href="#" className="text-white font-bold bg-green-500 mt-3 py-2 px-3 border-0 rounded text-lg hover:bg-green-400 duration-300">詳細はこちら</button>
+              <button href="#" className="text-white font-bold bg-green-500 py-1.5 px-3 border-0 rounded text-lg hover:bg-green-400 duration-300">詳細はこちら</button>
             </div>
             <div className="md:w-1/2 lg:max-w-lg w-5/6">
-              <img src="/img/paper.png" alt="パンフレット" width={1080} height={1542} />
+              <img src="/img/icon.ico" alt="something" width={800} height={1300} />
             </div>
             </div>
         </section>
       <footer>
-          <div className='flex pt-7 pb-12 border-t border-green-200  w-full pl-[6vw] lg:pl-[10vw] text-[2vw] font-extrabold '>
+          <div className='flex pt-7 pb-12 border-t border-green-200  w-full pl-[6vw] lg:pl-[10vw] text-[2vw] '>
               <Link className="flex " href="/"><Image className="mr-1" src="/img/icon.ico" alt="icon" width={10} height={10}/>ホーム</Link>
               <Link className="flex ml-5" href="/about"><Image className="mr-1" src="/img/icon.ico" alt="icon" width={10} height={10}/>リリエンワイスについて</Link>
               <Link className="flex ml-5" href="/music"><Image className="mr-1" src="/img/icon.ico" alt="icon"  width={10} height={10}/>過去の演奏会の音源</Link>
